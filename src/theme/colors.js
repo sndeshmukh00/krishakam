a4323fe6570bde5d1a8de0962494895bf442954a
Ambalika252252 <amba25.official@gmail.com>
2023-06-29 22:48:42 +0530

    initial commit

---
+++ src/theme/colors.js
@@ -0,0 +1,16 @@
+const colors = {
+    primary: '#299750',
+    secondary: 'green',
+    textPrimary: '#000',
+    textSecondary: '#333',
+    textGrey: '#B4B4B4',
+    borderColor: '#D4D4D4',
+    backgroundColor: '#E4E4E4',
+    progressGrey: '#C1C1C1',
+    white: '#FFFFFF',
+    black: '#000000',
+    gray: '#F9F9F9'
+
+}
+
+export default colors;
