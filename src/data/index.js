7c83f80c231e6e35da891792fffb27d582d0733b
sndeshmukh00 <shanu05.official@gmail.com>
2023-07-24 23:04:32 +0530

    [Add]: Adding dummy data and utils

---
+++ src/data/index.js
@@ -0,0 +1,15 @@
+import Rice from '../assets/pngs/rice.png'
+import Wheat from '../assets/pngs/wheat.png'
+import Tomato from '../assets/pngs/tomato.png'
+import Banana from '../assets/pngs/banana.png'
+import Brinjal from '../assets/pngs/brinjal.png'
+
+const crops = [
+    { id: 1, name: 'Rice', details: 'Details of Crop 1', bgColor: '#b8e0d2', imageUrl: Rice },
+    { id: 2, name: 'Wheat', details: 'Details of Crop 2', bgColor: '#fcf4dd', imageUrl: Wheat },
+    { id: 3, name: 'Tomato', details: 'Details of Crop 3', bgColor: '#fce1e4', imageUrl: Tomato },
+    { id: 4, name: 'Banana', details: 'Details of Crop 4', bgColor: '#dcd5c7', imageUrl: Banana },
+    { id: 5, name: 'Brinjal', details: 'Details of Crop 5', bgColor: '#dfb2f4', imageUrl: Brinjal },
+];
+
+export default crops;
